0e3f9603931fa99bc3207a27c6d23ec433ddf6d0
Jacob Hummer <jcbhmr@outlook.com>
2023-11-02 14:00:22 -0500

    try this

--- _main.mjs
+++ _main.mjs
@@ -2,7 +2,7 @@ import { spawn } from "node:child_process";
 import { dirname, join } from "node:path";
 import { once } from "node:events";
 const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
-const subprocess = spawn(`'${file}'`, {
+const subprocess = spawn(`"${file.replaceAll(/(["'$`\\])/g, "\\$1")}"`, {
   shell: "bash",
   stdio: "inherit",
 });
