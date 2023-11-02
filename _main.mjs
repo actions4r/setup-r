289d39ac73d3d2315e6aa9d9efd428bceb925462
Jacob Hummer <jcbhmr@outlook.com>
2023-11-02 13:05:12 -0500

    7 lines

--- _main.mjs
+++ _main.mjs
@@ -1,13 +1,7 @@
 import { spawn } from "node:child_process";
 import { dirname, join } from "node:path";
 import { once } from "node:events";
-const main = "main.ts"; // 👈 CHANGE ME!
-const file = join(dirname(process.argv[1]), main);
-const subprocess = spawn(file, {
-  shell: "bash",
-  stdio: "inherit",
-});
+const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
+const subprocess = spawn(file, { shell: "bash", stdio: "inherit" });
 await once(subprocess, "spawn");
-subprocess.on("exit", (exitCode) => {
-  process.exit(exitCode);
-});
+subprocess.on("exit", (x) => process.exit(x));
