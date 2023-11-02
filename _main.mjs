7a5edde75858d293cb599f07917404ff011d405b
Jacob Hummer <jcbhmr@outlook.com>
2023-11-02 13:55:48 -0500

    try quoting

--- _main.mjs
+++ _main.mjs
@@ -2,6 +2,9 @@ import { spawn } from "node:child_process";
 import { dirname, join } from "node:path";
 import { once } from "node:events";
 const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
-const subprocess = spawn(file, { shell: "bash", stdio: "inherit" });
+const subprocess = spawn(`'${file}'`, {
+  shell: "bash",
+  stdio: "inherit",
+});
 await once(subprocess, "spawn");
 subprocess.on("exit", (x) => process.exit(x));
